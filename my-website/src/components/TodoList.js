640a6136a263a484671bf8530857c9bd6eeac946
Patrick <nekelpatrick@gmail.com>
2020-10-31 00:49:24 -0300

    adicionei os features CHECK, DELETE e FILTErs, também implementei a primeira versão de armazenamento da lista.

--- my-website/src/components/TodoList.js
+++ my-website/src/components/TodoList.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Todo from "./Todo.jsx";
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        <Todo />
+        {filteredTodos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
+        ))}
       </ul>
     </div>
   );
